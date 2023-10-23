16c4a4b3e446282ae99314580c0bd75447f9a4e7
codeyron <92419200+Nirshat@users.noreply.github.com>
2023-10-23 15:40:27 +0800

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <>
-      <div className="text-slate-500 pb-4 text-center">
+      <div className="text-slate-500 pt-8 pb-4 text-center">
         <span>© Developed by aronpaulgonzales. All right reserved.</span>
       </div>
     </>
